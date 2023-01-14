985aa129a7fbcca8e8d720a3d2e97cdc211ff1c8
Yasin <yasincoban1998@outlook.com>
2023-01-15 00:01:47 +0300

    StatisticPage charts

--- client/src/pages/StatisticPage.jsx
+++ client/src/pages/StatisticPage.jsx
@@ -1,13 +1,101 @@
 import React from "react";
-
+import { useEffect } from "react";
+import { useState } from "react";
+import { Area, Pie } from "@ant-design/plots";
 import Header from "../components/header/Header";
 import StatisticCard from "../components/statistics/StatisticCard";
 
 const StatisticPage = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    asyncFetch();
+  }, []);
+
+  const asyncFetch = () => {
+    fetch(
+      "https://gw.alipayobjects.com/os/bmw-prod/360c3eae-0c73-46f0-a982-4746a6095010.json"
+    )
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => {
+        console.log("fetch data failed", error);
+      });
+  };
+  const config = {
+    data,
+    xField: "timePeriod",
+    yField: "value",
+    xAxis: {
+      range: [0, 1],
+    },
+  };
+  const data2 = [
+    {
+      type: "分类一",
+      value: 27,
+    },
+    {
+      type: "分类二",
+      value: 25,
+    },
+    {
+      type: "分类三",
+      value: 18,
+    },
+    {
+      type: "分类四",
+      value: 15,
+    },
+    {
+      type: "分类五",
+      value: 10,
+    },
+    {
+      type: "其他",
+      value: 5,
+    },
+  ];
+  const config2 = {
+    appendPadding: 10,
+    data: data2,
+    angleField: "value",
+    colorField: "type",
+    radius: 1,
+    innerRadius: 0.6,
+    label: {
+      type: "inner",
+      offset: "-50%",
+      content: "{value}",
+      style: {
+        textAlign: "center",
+        fontSize: 14,
+      },
+    },
+    interactions: [
+      {
+        type: "element-selected",
+      },
+      {
+        type: "element-active",
+      },
+    ],
+    statistic: {
+      title: false,
+      content: {
+        style: {
+          whiteSpace: "pre-wrap",
+          overflow: "hidden",
+          textOverflow: "ellipsis",
+        },
+        content: "AntV\nG2Plot",
+      },
+    },
+  };
   return (
     <div>
       <Header />
-      <div className="px-6">
+      <div className="px-6 md:pb-0 pb-20">
         <div className="text-4xl font-bold text-center mb-4">İstatistikler</div>
 
         <div className="statistic-section">
@@ -32,6 +120,15 @@ const StatisticPage = () => {
               amount="10"
               img="/images/product.png"
             />
+          </div>
+
+          <div className="flex justify-between gap-10 lg:flex-row flex-col items-center">
+            <div className="lg:w-1/2 lg:h-full h-72">
+              <Area {...config} />
+            </div>
+            <div>
+              <Pie {...config2} />
+            </div>
           </div>
         </div>
       </div>
