00cbba39b95324047b6e641476d673faede70d23
Yasin <yasincoban1998@outlook.com>
2023-01-14 23:34:28 +0300

    StatisticPage

--- client/src/pages/StatisticPage.jsx
+++ client/src/pages/StatisticPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Header from "../components/header/Header";
+import StatisticCard from "../components/statistics/StatisticCard";
 
 const StatisticPage = () => {
   return (
@@ -8,8 +9,30 @@ const StatisticPage = () => {
       <Header />
       <div className="px-6">
         <div className="text-4xl font-bold text-center mb-4">İstatistikler</div>
-        <div className="text-xl">
-            <h2>Hoş geldin <span className="text-green-700 font-bold text-large">Admin</span>.</h2>
+
+        <div className="statistic-section">
+          <div className="statistic-cards grid xl:grid-cols-4 md:grid-cols-2 my-10 md:gap-10 gap-4">
+            <StatisticCard
+              title="Toplam Müşteri"
+              amount="10"
+              img="/images/user.png"
+            />
+            <StatisticCard
+              title="Toplam Kazanç"
+              amount="1500 TL"
+              img="/images/money.png"
+            />
+            <StatisticCard
+              title="Toplam Satış"
+              amount="50"
+              img="/images/sale.png"
+            />
+            <StatisticCard
+              title="Toplam Ürün"
+              amount="10"
+              img="/images/product.png"
+            />
+          </div>
         </div>
       </div>
     </div>
